03053225519a567a9c0542152c60dffc095dde39
Izerex12 <153672637+Izerex12@users.noreply.github.com>
2026-03-05 00:42:45 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Открытие модального окна */
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+/* Закрытие модального окна */
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
